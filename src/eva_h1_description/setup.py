e3df0c94639659315bcf69101f5b67df4805657e
figgy <sahasra7733@gmail.com>
2026-07-06 14:53:21 +0530

    Phase 2: Stable EVA-H1 URDF with Gazebo physics

--- src/eva_h1_description/setup.py
+++ src/eva_h1_description/setup.py
@@ -10,12 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/display.launch.py']),
-        ('share/' + package_name + '/urdf', [
-            'urdf/eva_h1.urdf.xacro',
-            'urdf/eva_h1.urdf'
-        ]),
-     ],
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sahasra',
